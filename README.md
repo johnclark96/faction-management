ef0187fa57ba7483c73528edd4646547658e235e
JClark <jclarkgaming01@gmail.com>
2022-02-23 20:00:00 -0500

    Minor Updates and Setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # faction-management
+
+## Planned Features
+ - Faction List
+ - Faction Leaders
+ - FM UI (To manage everything from a UI)
+ - Ranking
+ - Bank/Economy System
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'johnclark96'
+description 'Faction Management System to Manage Factions in game'
+version '1.0.0'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+ui_page 'html/index.html'
