6970eed82cb350d92c22081e3892b5f2a3752e3c
JClark <mlitvinkox96@gmail.com>
2022-02-25 19:18:46 -0500

    Trying to make a UI

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,46 @@
+local menu = false
+
+RegisterCommand("faction", function(source)
+    SetMenu(not menu)
+end)
+
+RegisterNUICallback('main', function(data)
+    chat(data.text, {0, 255, 0})
+    SetMenu(false)
+
+end)
+
+RegisterNUICallback('error', function(data)
+    chat(data.error, {255, 0, 0})
+    SetMenu(false)
+end)
+
+RegisterNUICallback('exitMenu', function(data)
+    chat("Terminated Menu", {0, 255, 0})
+    SetMenu(false)
+end)
+
+Citizen.CreateThread(function()
+
+end)
+
+function SetMenu(bool)
+    menu = bool
+    SetNuiFocus(bool, bool)
+    SendNuiMessage({
+        type='menu',
+        status = bool,
+    })
+end
+
+function chat(str, color)
+    TriggerEvent('chat:addMessage', {
+        color = color,
+        multiline = true,
+        args = {str}
+    })
+end
+
 RegisterCommand('factions', function()
     msg('List all Factions')
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,3 +10,10 @@ client_script 'client/main.lua'
 server_script 'server/main.lua'
 
 ui_page 'html/index.html'
+
+files {
+    "html/index.html",
+    "html/index.js",
+    "html/index.css",
+    "html/reset.css"
+}
